2b8430c23f5628e188eb10f922e41578a27c2212
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-03 13:22:51 +0200

    Removed unused imports

--- src/main/java/es/fercbrt/springbootweb/controllers/UserRestController.java
+++ src/main/java/es/fercbrt/springbootweb/controllers/UserRestController.java
@@ -3,11 +3,7 @@ package es.fercbrt.springbootweb.controllers;
 import es.fercbrt.springbootweb.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("api")

7e3b697077eed248c1905251ec6815f6b3dc958e
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-03 14:17:47 +0200

    Learned how to use the @PostMapping and @RequestBody annotations

--- src/main/java/es/fercbrt/springbootweb/controllers/PathVariableController.java
+++ src/main/java/es/fercbrt/springbootweb/controllers/PathVariableController.java
@@ -1,10 +1,8 @@
 package es.fercbrt.springbootweb.controllers;
 
+import es.fercbrt.springbootweb.models.User;
 import es.fercbrt.springbootweb.models.dto.ParamDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -20,5 +18,10 @@ public class PathVariableController {
     @GetMapping("/mix/{product}/{id}")
     public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
         return Map.of("product", product, "id", id);
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        return user;
     }
 }

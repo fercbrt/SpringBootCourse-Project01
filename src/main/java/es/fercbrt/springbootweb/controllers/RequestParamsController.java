d5764820be16e0590e863268a310b615c28f8424
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-03 14:05:34 +0200

    Learned how to use the HttpServletRequest object

--- src/main/java/es/fercbrt/springbootweb/controllers/RequestParamsController.java
+++ src/main/java/es/fercbrt/springbootweb/controllers/RequestParamsController.java
@@ -1,6 +1,7 @@
 package es.fercbrt.springbootweb.controllers;
 
 import es.fercbrt.springbootweb.models.dto.ParamDto;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,5 +19,14 @@ public class RequestParamsController {
     @GetMapping("/bar")
     public ParamDto bar(@RequestParam(required = false, defaultValue = "Hello world!", name = "text") String text, @RequestParam(required = false, defaultValue = "42", name = "code") int code) {
         return new ParamDto(text, code);
+    }
+
+    @GetMapping("/request")
+    public ParamDto request(HttpServletRequest request) {
+        try {
+            return new ParamDto(request.getParameter("message"), Integer.parseInt(request.getParameter("code")));
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

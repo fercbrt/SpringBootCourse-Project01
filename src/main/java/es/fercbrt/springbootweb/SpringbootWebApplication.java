f917e80a26722dffe366c1347948a35bd4ce16ae
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-03 16:00:51 +0200

    Migrated properties from application.properties to an external values.properties file

--- src/main/java/es/fercbrt/springbootweb/SpringbootWebApplication.java
+++ src/main/java/es/fercbrt/springbootweb/SpringbootWebApplication.java
@@ -2,8 +2,13 @@ package es.fercbrt.springbootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+        @PropertySource("classpath:values.properties")
+})
 public class SpringbootWebApplication {
 
     public static void main(String[] args) {

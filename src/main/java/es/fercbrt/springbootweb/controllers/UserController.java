785b9035bbd59c5b285f6295d3939a87b2af9377
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-03 13:50:23 +0200

    Created a new list method on the UserController and learned how to iterate with ThyMeLeaf

--- src/main/java/es/fercbrt/springbootweb/controllers/UserController.java
+++ src/main/java/es/fercbrt/springbootweb/controllers/UserController.java
@@ -3,7 +3,11 @@ package es.fercbrt.springbootweb.controllers;
 import es.fercbrt.springbootweb.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -14,5 +18,19 @@ public class UserController {
         model.addAttribute("title", "User Details");
         model.addAttribute("user", user);
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = new ArrayList<>(
+                List.of(
+                        new User("Fernando", "Calvino", "user@example.com"),
+                        new User("John", "Doe", "user@example.com"),
+                        new User("Jane", "Doe", "user@example.com")
+                )
+        );
+        model.addAttribute("title", "User List");
+        model.addAttribute("users", users);
+        return "list";
     }
 }
